25ad5688e021afb9e1369e44d8a522772dbf1e18
Mikatoshi <230616882+GSF-001@users.noreply.github.com>
2026-04-03 13:59:05 +0700

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ height: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
+      <a href="/login" style={{ padding: 10, background: "cyan", color: "#000" }}>
+        MASUK GSF
+      </a>
+    </div>
+  );
+}
